ed19553d5ec5bfbe505acca7f55ce9620f898ff8
groverliam <groverliam@gmail.com>
2019-04-13 17:04:28 -0400

    de

--- index.php
+++ index.php
@@ -192,13 +192,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						$('#ca-container').contentcarousel();
 					</script>
 		</div> -->
-		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>SE357</title>
-<link rel="stylesheet" href="includes/admin.css" type="text/css" media="screen" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-</head>
 
 	<div id="content" >	
 		<h1>View Products</h1>		
